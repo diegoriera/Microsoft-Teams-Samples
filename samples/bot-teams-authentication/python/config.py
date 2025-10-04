042948d2a59f986319150d7d4ae0cc6529799f0f
Diego Riera <dirierat@microsoft.com>
2025-10-04 21:23:32 +0100

    test pre-commit hook

--- samples/bot-teams-authentication/python/config.py
+++ samples/bot-teams-authentication/python/config.py
@@ -1,19 +1,21 @@
-#!/usr/bin/env python3
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-import os
-
-""" Bot Configuration """
-
-
-class DefaultConfig:
-    """ Bot Configuration """
-
-    PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "6616c210-1d64-4777-97bb-67dac47968a7")
-    #APP_ID = os.environ.get("MicrosoftAppId", "b7776e64-4fb7-4fbe-bfc1-69893eb71aa4")
-    #APP_TYPE = os.environ.get("MicrosoftAppType", "SingleTenant")
-    APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
-    APP_TENANTID = os.environ.get("MicrosoftAppTenantId", "e7db9eb6-7162-4d1c-a769-4834407d6c3c")
-    CONNECTION_NAME = os.environ.get("ConnectionName", "OAuth")
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+""" Bot Configuration """
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = os.environ.get("MicrosoftAppId", "<REDACTED>")
+    #APP_ID = os.environ.get("MicrosoftAppId", "<REDACTED>")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "<REDACTED>")
+    #APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "<REDACTED>")
+    #APP_TYPE = os.environ.get("MicrosoftAppType", "SingleTenant")
+    APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
+    APP_TENANTID = os.environ.get("MicrosoftAppTenantId", "<REDACTED>")
+    CONNECTION_NAME = os.environ.get("ConnectionName", "<REDACTED>")
